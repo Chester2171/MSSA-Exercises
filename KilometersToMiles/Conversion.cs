b4b94bb6ac1b6f910bcedc36093bb9918af85244
Chester2171 <Chester2171@gmail.com>
2020-08-20 09:20:06 -0700

    ConversionsMi&Km

---
+++ KilometersToMiles/Conversion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KilometersToMiles
+{
+    class Conversion
+    {
+        public static double KilometersToMiles(double input)
+        {
+            input = input / 1.609;
+            return Math.Round(input, 2);
+        }
+        public static double MilesToKilometers(double input)
+        {
+            input = input * 1.609;
+            return Math.Round(input, 2);
+        }
+    }
+}
---
+++ KilometersToMiles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace KilometersToMiles
+{
+    class Program
+    {
+        static double Distance()
+        {
+            double input = 0;
+            Console.WriteLine("Enter a distance to see it in both Kilometers and Miles: ");
+            try
+            {
+                input = double.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return Distance();
+            }
+            return input;
+        }
+        
+        static void Main(string[] args)
+        {
+            double input = Distance();
+            Console.WriteLine($"{input} Kilometers converted to Miles is {Conversion.KilometersToMiles(input)}");
+            Console.WriteLine($"{input} Miles converted to Kilometers is {Conversion.MilesToKilometers(input)}");
+        }
+    }
+}
