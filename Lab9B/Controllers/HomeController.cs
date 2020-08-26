c8f1b95a2600f3798bba0dcd1e2ebeef67f8fc0a
Chester2171 <Chester2171@gmail.com>
2020-08-25 17:06:31 -0700

    Lab 9B

---
+++ Lab9B/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab9B.Models;
+
+namespace Lab9B.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public ViewResult Index()
+        //{
+        //    Dictionary<string, Product> products = new Dictionary<string, Product> 
+        //    {
+        //        { "Kayak", new Product { Name = "Kayak", Price = 275M } },
+        //        { "Lifejacket", new Product{ Name = "Lifejacket", Price = 48.95M } }
+        //    };
+        //    return View("Index", products.Keys);
+        //}
+        public ViewResult Index()
+        {
+            return View(new string[] { "Cody", "Thomas", "Chester" });
+        }
+    }
+}
