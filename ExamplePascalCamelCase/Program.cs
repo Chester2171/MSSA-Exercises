ccbbe9f6747fb5ca05d6a8739e812180ac6999f2
Chester2171 <Chester2171@gmail.com>
2020-10-28 18:39:51 -0700

    Classwork

---
+++ ExamplePascalCamelCase/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Whiteborad
+{
+    class Program
+    {
+        public static string CamelCase(string[] input)
+        {
+            string result = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = input[i].ToLower();
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    if((input[i][j] < 'a' || input[i][j] > 'z') && input[i][j] != ' ')
+                    {
+                        continue;
+                    }
+                    while(input[i][j] == ' ')
+                    {
+                        j++;
+                        if(input[i][j] < 'a' || input[i][j] > 'z')
+                        {
+                            result += Char.ToUpper(input[i][j + 1]);
+                        }
+                    }
+                    if (j == 0 && i != 0)
+                    {
+                        result += Char.ToUpper(input[i][0]);
+                    }
+                    else
+                    {
+                        result += input[i][j];
+                    }
+                }
+            }
+            return result;
+        }
+        public static string PascalCase(string[] input)
+        {
+            string result = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = input[i].ToLower();
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    if (input[i][j] < 'a' || input[i][j] > 'z')
+                    {
+                        continue;
+                    }
+                    if (j == 0)
+                    {
+                        result += Char.ToUpper(input[i][0]);
+                    }
+                    else
+                    {
+                        result += input[i][j];
+                    }
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CamelCase(new string[] { "hello", "RichaRd", "HOW", "aR!E", "yOu" }));
+            Console.WriteLine(PascalCase(new string[] { "hello", "RichaRd", "HOW", "aR!E", "yOu" }));
+        }
+    }
+}
